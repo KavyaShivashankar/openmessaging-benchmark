06eebbfe20efd54e161e78424610febae60f8d6f
Kavya Shivashankar <Kavya.Shivashankar@redpanda.com>
2024-10-29 10:40:22 -0700

    Added a script to change the node_status_interval config in redpanda

--- ks-scripts/set_node_interval_status.sh
+++ ks-scripts/set_node_interval_status.sh
@@ -9,7 +9,7 @@ BRKR1="35.165.40.94"
 
 echo "Setting  omb benchmark worker service on $WRKR1, $WRKR2, $WRKR3, $WRKR4"
 
-ssh -i ~/.ssh/redpanda_aws ubuntu@${BRKR1} -t "sudo rpk set cluster config set node_status_interval 500"
+ssh -i ~/.ssh/redpanda_aws ubuntu@${BRKR1} -t "sudo rpk cluster config set node_status_interval 500"
 
 echo "Done"
 echo ""
